48c9629a329ba076958d1767cbc5e4d07256d100
Dennis Hickox <frozzel@me.com>
2025-05-19 23:47:48 -0400

    building python movie app init

--- main.py
+++ main.py
@@ -17,14 +17,64 @@ app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 Bootstrap5(app)
 
 # CREATE DB
+class Base(DeclarativeBase):
+    pass
+
+db = SQLAlchemy(model_class=Base)
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///movie.db"
+# initialize the app with the extension
+db.init_app(app)
 
 
 # CREATE TABLE
+class Movie(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), nullable=False, unique=True)
+    year: Mapped[int] = mapped_column(Integer, nullable=False)
+    description: Mapped[str] = mapped_column(String(500), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
+    ranking: Mapped[int] = mapped_column(Integer, nullable=False)
+    review: Mapped[str] = mapped_column(String(500), nullable=False)
+    img_url: Mapped[str] = mapped_column(String(500), nullable=False)
+    
+with app.app_context():
+    db.create_all()
+    
+# with app.app_context():
+#     new_movie = Movie(
+#     title="Phone Booth",
+#     year=2002,
+#     description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
+#     rating=7.3,
+#     ranking=10,
+#     review="My favourite character was the caller.",
+#     img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+# )
+#     db.session.add(new_movie)
+#     db.session.commit()
 
+# with app.app_context():
+#     second_movie = Movie(
+#     title="Avatar The Way of Water",
+#     year=2022,
+#     description="Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
+#     rating=7.3,
+#     ranking=9,
+#     review="I liked the water.",
+#     img_url="https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg"
+# )
+#     db.session.add(second_movie)
+#     db.session.commit()
+    
+    
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    # GET MOVIE DATA
+    result = db.session.execute(db.select(Movie).order_by(Movie.title))
+    all_movies = result.scalars().all()
+    return render_template("index.html", movies=all_movies)
 
 
 if __name__ == '__main__':
